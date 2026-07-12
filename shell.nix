ced787f14ae2923588442c6629952dcd8d2d0795
ghost <unknown@unknown.com>
2026-07-12 22:15:21 +0800

    chore: shell.nix

--- shell.nix
+++ shell.nix
@@ -2,34 +2,15 @@
   pkgs ? import <nixpkgs> {
     config = {
       allowUnfree = true;
-      android_sdk.accept_license = true;
     };
   },
-  ANDROID_HOME ? "$HOME/Android/Sdk",
 }:
 
-# let
-#   AndroidPkgs = pkgs.androidenv.composeAndroidPackages {
-#     includeNDK = true;
-#     includeEmulator = true;
-#     includeSystemImages = true;
-
-#     buildToolsVersions = [
-#       "latest"
-#     ];
-#     platformVersions = [ "36" ];
-#   };
-# in
 with pkgs;
 mkShell {
-  # shellHook = ''
-  #   export ANDROID_SDK_ROOT="${AndroidPkgs.androidsdk}/libexec/android-sdk";
-  #   export ANDROID_NDK_ROOT="$ANDROID_SDK_ROOT/ndk-bundle";
-
-  #   export ANDROID_HOME="$ANDROID_SDK_ROOT";
-  #   export NDK_HOME="$ANDROID_NDK_ROOT";
-  #   export WEBKIT_DISABLE_DMABUF_RENDERER=1;
-  # '';
+  shellHook = ''
+    export WEBKIT_DISABLE_DMABUF_RENDERER=1;
+  '';
 
   nativeBuildInputs = with pkgs; [
     pkg-config
